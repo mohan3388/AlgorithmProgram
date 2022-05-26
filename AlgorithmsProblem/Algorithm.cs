c2b44e993e6fb8dce81474a48f789a5d2664962e
mohan338 <sahumohan338@gmail.com>
2022-05-26 11:00:24 +0530

    [mohan]add:Binary Search in String

---
+++ AlgorithmsProblem/Algorithm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsProblem
+{
+   public class Algorithm
+    {
+        public int BinarySearch(String[] arr, String x)
+
+        {
+            int l = 0, r = arr.Length;
+            while (l <= r)
+            {
+                int m = l + (r - l) / 2;
+                int res = x.CompareTo(arr[m]);
+                if (res == 0)
+                {
+                    return m;
+                }
+                if (res > 0)
+                {
+                    l = m + 1;
+                }
+                else
+                {
+                    r = m - 1;
+                }
+            }
+                return -1;
+            }
+        
+       
+    }
+}
---
+++ AlgorithmsProblem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AlgorithmsProblem;
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Welcome to the program");
+        Console.WriteLine("1.Binary Serach Program\n2.Exit the program");
+        bool check = true;
+        while(check)
+        {
+            Console.WriteLine("Enter above option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    const string File_Path = @"D:\dotnet\Algoritmprogram\AlgorithmProgram\AlgorithmsProblem\List.txt";
+                    string words = File.ReadAllText(File_Path);
+                    string[] arr = words.Split(',');
+                    Console.WriteLine("Enter word to Search");
+                    string a =Console.ReadLine();
+                    Algorithm search = new Algorithm();
+                    int result = search.BinarySearch(arr, a);
+                    if (result == -1)
+                        Console.WriteLine("Element not present");
+                    else
+                        Console.WriteLine("Element found at "+ result);
+                    break;
+                default:
+                    check = false;
+                    break;
+            }
+        }
+    }
+}
