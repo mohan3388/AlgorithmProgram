fd8151d2a69188086d150f4cf9dab3f6d7856bfa
mohan338 <sahumohan338@gmail.com>
2022-05-28 13:56:56 +0530

    [mohan]add: Anagram program

---
+++ AlgorithmsProblem/Anagram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsProblem
+{
+    public class Anagram
+    {
+        public bool AnagramProblem(string Firstlength, string SecondLength)
+        {
+            var val1 = Firstlength.ToLower().ToArray();
+            var val2 = SecondLength.ToLower().ToArray();
+            Array.Sort(val1);
+            Array.Sort(val2);
+            return new string(val1) == new string(val2);
+        }
+    }
+}
--- AlgorithmsProblem/Program.cs
+++ AlgorithmsProblem/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(String[] args)
     {
         Console.WriteLine("Welcome to the program");
-        Console.WriteLine("1.Binary Serach Program\n2.Insertion Sort Program\n3.Bubble Sort\n4.Merge Sort Problem");
+        Console.WriteLine("1.Binary Serach Program\n2.Insertion Sort Program\n3.Bubble Sort\n4.Merge Sort Problem\n5.Anagram Problem");
         const string File_Path = @"D:\dotnet\Algoritmprogram\AlgorithmProgram\AlgorithmsProblem\List.txt";
         bool check = true;
         while(check)
@@ -49,6 +49,12 @@ class Program
                     merge.Sorting(arr4, 0, arr4.Length-1);
                     Console.WriteLine("unsorted array");
                     merge.DisplayMerge(arr4);
+                    break;
+                case 5:
+                    Anagram anagram=new Anagram();
+                    string firstname = "earth";
+                    string secondname = "heart";
+                    Console.WriteLine(anagram.AnagramProblem(firstname, secondname));
                     break;
                 default:
                     check = false;
